ff203cae2e73abb8e416e35222a4812590e130e1
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-06-08 00:06:07 +0800

    feat: Add more calculator features

--- js/app.js
+++ js/app.js
@@ -9,13 +9,14 @@ const divide = (a, b) => {
     if (a === 0 || b === 0) {
         return "Syntax Error"
     }
-    return a / b;
+    return String(a/b).includes(".") ? (a/b).toFixed(7) : (a/b);
 };
 
 let operand = "";
 let leftOperand = "";
 let operator = "";
 let rightOperand = "";
+let result = "";
 
 const operate = (a, operator, b) => {
     switch(operator){
@@ -37,24 +38,32 @@ buttons.addEventListener("click", (event) => {
 
         if (target.classList.contains("numbers")){
             operand = target.dataset.key;
-            display.textContent += operand;
+            if(result){
+                display.textContent = operand;   
+                result = ""; 
+            } else {
+                display.textContent += operand;
+            }
             console.log(operand);
         } else if (target.classList.contains("operation")) {
             switch (target.dataset.key) {
-                case "+":
-                    operator = target.dataset.key;
-                    leftOperand = +display.textContent;
-                    display.textContent = ""
-                    console.log(leftOperand);
-                    break;
                 case "=":
                     rightOperand = +display.textContent;
-                    console.log(rightOperand);
-                    console.log(leftOperand);
-                    console.log(add(leftOperand, rightOperand));
-                    console.log(operate(leftOperand, operator, rightOperand));
-                    display.textContent = operate(leftOperand, operator, rightOperand);
-
+                    // console.log(rightOperand);
+                    // console.log(leftOperand);
+                    // console.log(operator);
+                    result = operate(leftOperand, operator, rightOperand);
+                    display.textContent = result;
+                    break;
+                case "clear":
+                    display.textContent = "";
+                    break;
+                default:
+                    operator = target.dataset.key;
+                    console.log(operator)
+                    leftOperand = +display.textContent;
+                    display.textContent = ""
+                    break;
             }
         } 
     }
